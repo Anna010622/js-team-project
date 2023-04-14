129a4da69e873d91239632ce1a376731353e2d64
kuzkostia <112246522+kuzkostia@users.noreply.github.com>
2023-04-14 23:16:22 +0300

    add scroll-up

---
+++ src/js/scroll-up-btn.js
@@ -0,0 +1,16 @@
+const upBtn = document.querySelector('.up-btn');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 500) {
+    upBtn.style.display = 'block';
+  } else {
+    upBtn.style.display = 'none';
+  }
+});
+
+upBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  });
+});
