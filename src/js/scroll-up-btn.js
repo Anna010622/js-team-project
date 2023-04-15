9cfd30dd86dc24c189170f5c08fccddc927ff9e5
kuzkostia <112246522+kuzkostia@users.noreply.github.com>
2023-04-15 16:28:03 +0300

    fix scroll-up

--- src/js/scroll-up-btn.js
+++ src/js/scroll-up-btn.js
@@ -1,11 +1,13 @@
 const upBtn = document.querySelector('.up-btn');
-window.addEventListener('scroll', () => {
+window.addEventListener('scroll', scroll);
+
+function scroll() {
   if (window.scrollY > 500) {
     upBtn.style.display = 'block';
   } else {
     upBtn.style.display = 'none';
   }
-});
+}
 
 upBtn.addEventListener('click', () => {
   window.scrollTo({
