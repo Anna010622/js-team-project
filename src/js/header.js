550737df9e7d643c9ee1e907812dcf67c9911023
Taras <topanasenko85@gmail.com>
2023-04-16 02:51:55 +0300

    header & mobile menu Markup render

--- src/js/header.js
+++ src/js/header.js
@@ -1,52 +1,112 @@
-// const headerNav = document.querySelector('nav');
-// const headerButton = document.querySelector('.header__container');
+import BrowserSprite from '../images/icons.svg';
 
-// if (localStorage.getItem('key') !== null) {
-//   console.log(111);
-//   headerNav.innerHTML = `
-//   <ul class="nav">
-//     <li class="nav__item">
-//       <a href="/index.html" class="nav__link ">
-//       HOME
-//       </a>
-//     </li>
-//     <li class="nav__item">
-//       <a href="/shoppinglist.html" class="nav__link">
-//       SHOPPING LIST
-//         <svg class="icon_shop" width="13" height="16">
-//           <use href="./images/icons.svg#icon-user"></use>
-//         </svg>
-//       </a>
-//     </li>
-//   </ul>`;
-//   headerButton.insertAdjacentHTML(
-//     'beforeend',
-//     `<button type="button" class="js-open-modal button header__button" data-modal="buynow">
-//         Sign up
-//           <svg class="button__icon" width="16" height="16">
-//             <use href='./images/icons.svg#icon-trash''></use>
-//           </svg>
-//     </button>`
-//   );
+const headerNav = document.querySelector('nav');
+const headerButton = document.querySelector('.header__container');
+const mobileMenu = document.querySelector('.mobile-menu__flex');
 
-//   // headerNav.classList.add('hiden');
-//   // headerButton.classList.add('hiden');
-// } else {
-//   console.log(9999);
-//   headerButton.insertAdjacentHTML(
-//     'beforeend',
-//     `<button type="button" class="js-open-modal button header__button" data-modal="buynow">
-//     USER
-//       <svg svg class= "button__icon" width = "16" height = "16" >
-//         <use href="./images/icons.svg#icon-trash">
-//         </use> <
-//       /svg >
-//     </button > `
-//   );
+if (localStorage.getItem('key') !== null) {
+  console.log(111);
+  headerNav.insertAdjacentHTML(
+    'beforeend',
+    `<ul class="nav">
+        <li class="nav__item">
+          <a href="./index.html" class="nav__link">HOME</a>
+        </li>
+        <li class="nav__item">
+          <a href="./shoppinglist.html" class="nav__link"
+            >SHOPPING LIST
+            <svg class="icon_shop" width="20" height="20">
+              <use href=${BrowserSprite}#icon-shoppingcart></use>
+            </svg>
+          </a>
+        </li>
+      </ul>
+    `
+  );
+  headerButton.insertAdjacentHTML(
+    'beforeend',
+    `<button
+      type="button"
+      class="js-open-modal button header__button"
+      data-modal="buynow"
+    >
+      <svg class="button__icon" width="19" height="19">
+        <use href=${BrowserSprite}#icon-user></use>
+      </svg>
+      USER
+      <svg class="button__icon" width="23" height="26">
+        <use href=${BrowserSprite}#icon-arrow-down></use>
+      </svg>
+    </button>`
+  );
+  mobileMenu.insertAdjacentHTML(
+    'beforeend',
+    `<button
+        type="button"
+        class="js-open-modal button mobile__button log-in"
+        data-modal="buynow"
+      >
+        <svg class="button__icon mobile-button__icon" width="16" height="16">
+          <use href=${BrowserSprite}#icon-user></use>
+        </svg>
+        USER
+      </button>
 
-//   //   // headerNav.classList.remove('hiden');
-//   //   // headerButton.classList.remove('hiden');
-// }
+      <ul class="mobile-nav">
+        <li class="mobile-nav__item">
+          <a href="./index.html" class="mobile-nav__link">HOME</a>
+        </li>
+        <li class="mobile-nav__item">
+          <a href="./shoppinglist.html" class="mobile-nav__link"
+            >SHOPPING LIST
+            <svg class="icon_shop" width="13" height="16">
+              <use href=${BrowserSprite}#icon-shoppingcart></use>
+            </svg>
+          </a>
+        </li>
+      </ul>
+      <button
+        type="button"
+        class="js-open-modal button mobile__button log-out"
+        data-modal="buynow"
+      >
+        Log out
+        <svg class="button__icon" width="16" height="16">
+          <use href=${BrowserSprite}#icon-arrow-right></use>
+        </svg>
+      </button>`
+  );
+
+  // headerNav.classList.add('hiden');
+  // headerButton.classList.add('hiden');
+} else {
+  console.log(9999);
+  headerButton.insertAdjacentHTML(
+    'beforeend',
+    `<button type="button" class="js-open-modal button header__button" data-modal="buynow">
+        Sign up
+          <svg class="button__icon" width="20" height="20">
+            <use href=${BrowserSprite}#icon-arrow-right></use>
+          </svg>
+    </button>`
+  );
+  mobileMenu.insertAdjacentHTML(
+    'beforeend',
+    `<button
+        type="button"
+        class="js-open-modal button mobile__button"
+        data-modal="buynow"
+      >
+        Sign up
+        <svg class="button__icon" width="16" height="16">
+          <use href=${BrowserSprite}#icon-arrow-right>></use>
+        </svg>
+      </button> `
+  );
+
+  //   // headerNav.classList.remove('hiden');
+  //   // headerButton.classList.remove('hiden');
+}
 
 (function () {
   const a = document.querySelectorAll('.nav a');
