c25fd13f647f70bc78c043847c634fc6211062cc
DemoUser77 <katykatykaty001@gmail.com>
2023-04-17 15:18:52 +0300

    settings

---
+++ src/js/firebase-modal.js
@@ -0,0 +1,21 @@
+const openModalBtnEl = document.querySelector("[data-modal-open]");
+const closeModalBtnEl = document.querySelector("[data-modal-close]");
+const modalEl = document.querySelector("[data-modal]");
+const bodyEl = document.querySelector('body');
+
+
+openModalBtnEl.addEventListener("click", toggleModal);
+closeModalBtnEl.addEventListener("click", toggleModal);
+
+ function toggleModal() {
+    modalEl.classList.toggle("is-hidden");
+    bodyEl.classList.toggle('no-scroll');
+    
+}
+  
+window.addEventListener('keydown', (event) => {
+        if (event.key === "Escape") {
+           modalEl.classList.toggle("is-hidden")
+        }
+     
+    })
