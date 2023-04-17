135ba41c2dc1391b97e7ba4c4f27b85570b3e2a1
BieliakovS <93442308+BieliakovS@users.noreply.github.com>
2023-04-17 22:24:07 +0300

    bug-fixed-mobile-menu

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,19 +1,21 @@
-const toggleEl = document.querySelector('.switch__input');
+const toggleArray = document.querySelectorAll('.switch__input');
 const currentTheme = localStorage.getItem('theme');
 
-if (currentTheme === 'dark-theme') {
-  toggleEl.checked = true;
-  document.body.classList.add('dark');
-}
-
-function switchTheme(e) {
-  if (e.target.checked) {
+for (toggleEl of toggleArray) {
+  if (currentTheme === 'dark-theme') {
+    toggleEl.checked = true;
     document.body.classList.add('dark');
-    localStorage.setItem('theme', 'dark-theme');
-  } else {
-    document.body.classList.remove('dark');
-    localStorage.removeItem('theme');
   }
+  
+  function switchTheme(e) {
+    if (e.target.checked) {
+      document.body.classList.add('dark');
+      localStorage.setItem('theme', 'dark-theme');
+    } else {
+      document.body.classList.remove('dark');
+      localStorage.removeItem('theme');
+    }
+  }
+  
+  toggleEl.addEventListener('change', switchTheme);
 }
-
-toggleEl.addEventListener('change', switchTheme);
