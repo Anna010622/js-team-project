bbb9a0fee5f1a6a18bc4f9190e748b116b9d13bb
Taras <topanasenko85@gmail.com>
2023-04-18 21:01:44 +0300

    fix  header & firebase-modal

--- src/js/firebase-modal.js
+++ src/js/firebase-modal.js
@@ -1,32 +1,68 @@
-const closeModalBtnEl = document.querySelector("[data-modal-close]");
-const modalEl = document.querySelector("[data-modal]");
+import { all } from 'axios';
+
+const backdropFirebase = document.querySelector('.backdrop-firebase');
+const logoutButtonEl = document.querySelector('button[data-modal="logout"]');
+const logoutButtonMobileEl = document.querySelector(
+  'button[data-modal="logout-modal"]'
+);
+const submitModal = document.querySelector(
+  '.backdrop-firebase .modal-form-button'
+);
+
+const signupButtonEl = document.querySelector('button[data-modal="signup"]');
+const signupButtonModalEl = document.querySelector(
+  'button[data-modal="signup-modal"]'
+);
+// console.log(logoutButtonEl);
+//*** */
+const closeModalBtnEl = document.querySelector('[data-modal="close"]');
+const modalEl = document.querySelector('[data-modal]');
 const bodyEl = document.querySelector('body');
 const headerEl = document.querySelector('header');
-const signupButtonEl = document.querySelector('button[data-modal="singup"]');
 const modal = document.getElementById('modal');
 
-
-signupButtonEl.addEventListener("click", toggleModal);
-closeModalBtnEl.addEventListener("click", toggleModal);
-
- function toggleModal() {
-    modalEl.classList.toggle("is-hidden");
-    bodyEl.classList.toggle('no-scroll');
-    
+let nameUser = '';
+submitModal.addEventListener('click', localSet);
+function localSet() {
+  nameUser = document.querySelector('[id="user_name"]').value;
+  localStorage.setItem('Name', `${nameUser}`);
+  location.reload(all);
 }
-  
-const signInLink = document.querySelector('a[data-modal-open="#modal_2"]');
-const modal2 = document.getElementById('modal_2');
 
-signInLink.addEventListener('click', () => {
-  
-  modal.classList.toggle("is-hidden");
-  modal2.classList.toggle("is-hidden");
+if (signupButtonEl) {
+  signupButtonEl.addEventListener('click', toggleModal);
+  signupButtonModalEl.addEventListener('click', toggleModal);
+  closeModalBtnEl.addEventListener('click', toggleModal);
+  function toggleModal() {
+    backdropFirebase.classList.toggle('is-hidden');
+    bodyEl.classList.toggle('no-scroll');
+  }
+  return;
+}
+
+if (logoutButtonEl) {
+  logoutButtonEl.addEventListener('click', localRemove);
+  logoutButtonMobileEl.addEventListener('click', localRemove);
+  function localRemove() {
+    localStorage.removeItem('Name');
+    location.reload(all);
+  }
+  return;
+}
+// name = 'user_name';
+
+// const signInLink = document.querySelector('a[data-modal-open]');
+
+// const modal2 = document.getElementById('modal_2');
+
+// signInLink.addEventListener('click', () => {
+//   modal.classList.toggle('is-hidden');
+//   modal2.classList.toggle('is-hidden');
+// });
+
+window.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    modalEl.classList.toggle('is-hidden');
+    modal2.classList.toggle('is-hidden');
+  }
 });
-
-window.addEventListener('keydown', (event) => {
-    if (event.key === "Escape") {
-      modalEl.classList.toggle("is-hidden");
-      modal2.classList.toggle("is-hidden");
-        }
-})
