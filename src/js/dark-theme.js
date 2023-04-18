918e64033b8aa554f57a218da22d77f603413c75
Anna Trifonova <3809322999@ukr.net>
2023-04-18 10:19:58 +0300

    fix

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,27 +1,27 @@
 const toggleArray = document.querySelectorAll('.switch__input');
 const currentTheme = localStorage.getItem('theme');
 
-for (toggleEl of toggleArray) {
+for (let toggleEl of toggleArray) {
   if (currentTheme === 'dark-theme') {
     toggleEl.checked = true;
     document.body.classList.add('dark');
   }
-  
+
   function switchTheme(e) {
     if (e.target.checked) {
       document.body.classList.add('dark');
       localStorage.setItem('theme', 'dark-theme');
-      for(let el of toggleArray) {
+      for (let el of toggleArray) {
         el.checked = true;
       }
     } else {
       document.body.classList.remove('dark');
       localStorage.removeItem('theme');
-      for(let el of toggleArray) {
+      for (let el of toggleArray) {
         el.checked = false;
       }
     }
   }
-  
+
   toggleEl.addEventListener('change', switchTheme);
 }
