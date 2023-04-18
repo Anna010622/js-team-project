ae594e68b173666123225752e063cbe52383ec47
tTanya98 <chichkinat98@gmail.com>
2023-04-18 09:49:49 +0300

    updt

--- src/js/modals.js
+++ src/js/modals.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { toLocalStorage } from './local-storage-set';
+import { createBookMarkup } from './create-book-markup';
 import amazon1 from '../images/modal/amazon1@1x.png';
 import amazon2 from '../images/modal/amazon2@2x.png';
 import apple1 from '../images/modal/apple1@1x.png';
@@ -8,8 +9,9 @@ import bookshop1 from '../images/modal/bookshop1@1x.png';
 import bookshop2 from '../images/modal/bookshop2@2x.png';
 
 const backdropEl = document.querySelector('[data-modal]');
-const modalMainEl = document.querySelector('.modal-books-container');
+// const modalMainEl = document.querySelector('.modal-books-container');
 const modalBookEl = document.querySelector('.modal-books-wrapper');
+const openModalBook = document.querySelector('[data-modal-open]');
 const closeModalBook = document.querySelector('[data-modal-close]');
 const addBookBtn = document.querySelector('.modal__add-btn');
 const booksSectionEl = document.querySelector('.books-section');
@@ -36,11 +38,13 @@ function getBookById(bookId) {
   const URL = `https://books-backend.p.goit.global/books/${bookId}`;
 
   return axios.get(URL);
-}
+} 
 
+openModalBook.addEventListener('click', toggleModal);
 closeModalBook.addEventListener('click', toggleModal);
-function toggleModal() {
-  const {book_image, title, author, description } = book;
+
+function toggleModal(bookId) {
+  const {book_image, title, author, description} = bookId;
 
   const markup = `  
   <div class="book__img-wrapper"> 
@@ -52,33 +56,17 @@ function toggleModal() {
           <p class="book__about">${description}</p> 
         </div> 
         <div> 
-        <ul class="book-shops"> 
-        <li class="book-shops__item"> 
-          <a class="book-shops__link" href="${buy_links.find(link => link.name === 'Amazon')}" target="_blank" rel="noopener noreferrer" aria-label="Amazon"> 
-          <img class="book-shops__img" srcset=" ${amazon1} 1x, ${amazon2} 2x "src="${amazon1}" alt="Amazon" width="62" height="19"> 
-          </a> 
-        </li> 
-        <li class="book-shops__item"> 
-          <a class="book-shops__link" href="${buy_links.find(link => link.name === 'Apple Books')}" target="_blank" rel="noopener noreferrer" aria-label="Apple Books"> 
-          <img class="book-stores__img" srcset=" ${apple1} 1x, ${apple2} 2x "src="${apple1}" alt="Apple Books" width="33" height="32">
-          </a> 
-        </li> 
-        <li class="book-stores__item"> 
-          <a class="book-stores__link" href="${buy_links.find(link => link.name === 'Bookshop')}" target="_blank" rel="noopener noreferrer" aria-label="Bookshop"> 
-          <img class="book-stores__img" srcset=" ${bookshop1} 1x, ${bookshop2} 2x "src="${bookshop1}" alt="Bookshops" width="38" height="36">
-          </a> 
-        </li> 
-      </ul> 
         </div> 
       </div> 
   </div> `; 
 
-  booksSectionEl.classList.toggle('modal-open');
-  backdropEl.classList.toggle('visually-hidden');
   modalBookEl.insertAdjacentHTML('afterbegin', markup); 
+  backdropEl.classList.toggle('is-hidden');
+  booksSectionEl.classList.toggle('modal-open');
   showModal();
   addBookBtn.addEventListener('click', addToShoppingList(book))
 }
+
 const underBtnText = document.createElement('p');
 function addToShoppingList(book) { 
   let oneBook = { ...book }; 
